45ca86ac600a19783a55331f155e2bcc89a88a2d
Lizard-git <76644328+Lizard-git@users.noreply.github.com>
2023-06-15 02:53:33 +0900

    add minor change

--- src/main/java/sfr/application/corporateportal/portal/controller/rest/NewsRestController.java
+++ src/main/java/sfr/application/corporateportal/portal/controller/rest/NewsRestController.java
@@ -3,6 +3,7 @@ package sfr.application.corporateportal.portal.controller.rest;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 import sfr.application.corporateportal.portal.entity.NewsEntity;
 import sfr.application.corporateportal.portal.service.NewsService;
@@ -17,6 +18,6 @@ public class NewsRestController {
     @ResponseBody
     public Page<NewsEntity> GetAllUsers(@PathVariable Integer page) {
 
-        return newsService.getAllNews(PageRequest.of(page, 5));
+        return newsService.getAllNews(PageRequest.of(page, 5, Sort.by("creationDate").descending()));
     }
 }
